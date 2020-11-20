c7fb299b5605165873895b648c0e9b14a15daab8
Courtney <courtneywalsh05@gmail.com>
2020-11-20 16:23:36 -0500

    registration completed. made email optional. Added registration link to login page.

--- cwalshcode/register/views.py
+++ cwalshcode/register/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 # from django.contrib.auth import login, authenticate
 from .forms import RegisterForm
+from django.contrib import messages
 # from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def register(response):
@@ -8,7 +9,7 @@ def register(response):
 		form = RegisterForm(response.POST)
 		if form.is_valid():
 			form.save()
-		return redirect("/")
+			return redirect("/")
 	else:
 		form = RegisterForm()
 	return render(response, "register/register.html", {"form":form})
